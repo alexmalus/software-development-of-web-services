979a2464c00e22b7559306f03519b7f9b8d76e3f
Alexandru Malus <malus.alexandru@yahoo.com>
2015-11-17 23:05:17 +0100

    LameDuck FlightDB update, GetFlights

--- LameDuck/src/java/lameduck/ws/LameDuckService.java
+++ LameDuck/src/java/lameduck/ws/LameDuckService.java
@@ -8,6 +8,8 @@ package lameduck.ws;
 import java.util.ArrayList;
 import java.util.List;
 import javax.jws.WebService;
+import javax.xml.datatype.XMLGregorianCalendar;
+import lameduck.ws.FlightsDatabase.DateUtils;
 import ws.lameduck.FlightInfoType;
 import ws.lameduck.GetFlightsResponse;
 
@@ -17,16 +19,33 @@ import ws.lameduck.GetFlightsResponse;
  */
 @WebService(serviceName = "LameDuckWSDLService", portName = "LameDuckBindingPort", endpointInterface = "ws.lameduck.LameDuck", targetNamespace = "http://LameDuck.ws", wsdlLocation = "WEB-INF/wsdl/LameDuckService/LameDuckWSDL.wsdl")
 public class LameDuckService {
+//    used when we comunicate with the Bank Service
     static final int GROUP_NUMBER = 16;
-    private List<FlightInfoType> bookedFlights = new ArrayList();
+//    used when we add flights
+    private List<FlightInfoType> booked_flights = new ArrayList();
+//    used for transactions with the Bank Service
     static final String lameduck_acc_no = "23212312";
     
     public ws.lameduck.GetFlightsResponse getFlights(ws.lameduck.GetFlightsRequest request) {
-        GetFlightsResponse matchedFlights = new GetFlightsResponse();
+        GetFlightsResponse matched_flights = new GetFlightsResponse();
+        FlightsDatabase flight_db = new FlightsDatabase();
         
-//        FlightInfoType newItem;
-//        matchedFlights.getFlightInfoList().add(newItem);
-        return matchedFlights;        
+        for (FlightInfoType flight_info : flight_db.flight_info_list.getFlightInfoList()) {
+            int day = flight_info.getFlight().getLiftoffDate().getDay();
+            int month = flight_info.getFlight().getLiftoffDate().getMonth();
+            int year = flight_info.getFlight().getLiftoffDate().getYear();
+            XMLGregorianCalendar flightDate = DateUtils.convertDateToGregCal(day, month, year);
+            
+            boolean datesSame = request.getDateFlight().toGregorianCalendar().equals(flightDate.toGregorianCalendar());
+            boolean startAirSame = flight_info.getFlight().getStartAirport().equals(request.getStartDestination());
+            boolean destAirSame = flight_info.getFlight().getDestinationAirport().equals(request.getFinalDestination());
+            
+            if (datesSame && startAirSame && destAirSame) {
+                matched_flights.getFlightInfoList().add(flight_info);
+            }
+        }
+        
+        return matched_flights;        
     }
 
     public ws.lameduck.BookFlightResponse bookFlight(ws.lameduck.BookFlightRequest request) throws ws.lameduck.BookFlightFault {
