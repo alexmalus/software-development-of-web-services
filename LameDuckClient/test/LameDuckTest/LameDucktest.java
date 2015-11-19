6a093b1b22a243fe78777ab274ddab9006c65309
Alexandru Malus <malus.alexandru@yahoo.com>
2015-11-19 16:18:16 +0100

    1.0alpha version of LameDuckService, Draft started on Tests

---
+++ LameDuckClient/test/LameDuckTest/LameDucktest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LameDuckTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alex
+ */
+public class LameDucktest {
+    
+    @Test
+    public void chargeCreditCard() {
+//        chargeCreditCard(GROUP_NUMBER, request.getCreditCardInfo(), flight_price, LameDuck_account);
+    }
+    
+}
