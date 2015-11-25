2085c3348c1907d1a45fcaaf1297a084ef31b599
Alexandru Malus <malus.alexandru@yahoo.com>
2015-11-25 17:34:56 +0100

    Add TravelGood schema, TravelGood WSDL update

---
+++ TravelGoodBPELClient/test/TravelGoodBPELClientTest/testP2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TravelGoodBPELClientTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alex
+ */
+public class testP2 {
+    
+    public testP2() {
+    }
+    
+}
