d61064b3bf89be38656d45e8c9cee660bd9c4fa8
Martin Bøje Rasmussen <martinboejerasmussen@gmail.com>
2015-11-28 18:23:39 +0100

    BPEL Create initerary progress

--- TravelGoodBPELClient/test/TravelGoodBPELClientTest/ClientTest.java
+++ TravelGoodBPELClient/test/TravelGoodBPELClientTest/ClientTest.java
@@ -18,91 +18,91 @@ import ws.travelgoodschema.GetItineraryResponse;
  * @author Alex
  */
 public class ClientTest {
-    
+
     @BeforeClass
     public static void setUpClass() {
-        
+
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
 //        reset itineraries which will also destroy their bookedFlights and bookedHotels
     }
-    
+
     @Test
-    public void create_itinerary_test(){
+    public void create_itinerary_test() {
 //        in this test we attempt to create an itinerary which have:
 //        itineraryId
 //        it has an itinerary status = unconfirmed, confirmed, cancelled
 //        bookedFlights
 //        bookedHotels
-        
+
 //        if it fails, it throws an ItineraryFault
-        
 //        to be sure that below tests will succeed or fail(they depend on a itinerary),
 //        after this test passes, create itinerary in setupClass
+        
         String itineraryID = createItinerary(true);
         System.out.print(itineraryID);
-        
+
         GetItineraryResponse response = new GetItineraryResponse();
         GetItineraryRequest request = new GetItineraryRequest();
         request.setItineraryID(itineraryID);
         response = getItinerary(request);
+        
+
     }
-    
+
     @Test
-    public void add_flight_success_test(){
+    public void add_flight_success_test() {
 //        to the created itinerary, attempt to add a flight to bookedFlights
     }
-    
+
     @Test
-    public void add_flight_fail_test(){
-        
+    public void add_flight_fail_test() {
+
     }
-    
+
     @Test
-    public void cancel_flight_success_test(){
+    public void cancel_flight_success_test() {
 //        to the created itinerary, attempt to cancel an already booked flight
 //        in order for this test to pass, we need to setup an itinerary and an already booked flight
     }
-    
+
     @Test
-    public void cancel_flight_fail_test(){
-        
+    public void cancel_flight_fail_test() {
+
     }
-    
+
     @Test
-    public void add_hotel_success_test(){
-        
+    public void add_hotel_success_test() {
+
     }
-    
+
     @Test
-    public void add_hotel_fail_test(){
-        
+    public void add_hotel_fail_test() {
+
     }
-    
+
     @Test
-    public void cancel_hotel_success_test(){
+    public void cancel_hotel_success_test() {
 //        to the created itinerary, attempt to cancel an already booked hotel
 //        in order for this test to pass, we need to setup an itinerary and an already booked hotel
     }
-    
+
     @Test
-    public void cancel_hotel_fail_test(){
-        
-    }
-    
-    @Test
-    public void cancel_planning_test(){
-//        destroy itinerary
-    }
-    
-    @Test
-    public void cancel_itinerary_test(){
-//        destroy itinerary if it fulfills the requirements
+    public void cancel_hotel_fail_test() {
+
     }
 
-    
+    @Test
+    public void cancel_planning_test() {
+//        destroy itinerary
+    }
+
+    @Test
+    public void cancel_itinerary_test() {
+//        destroy itinerary if it fulfills the requirements
+    }
 
     private static GetItineraryResponse getItinerary(ws.travelgoodschema.GetItineraryRequest part1) {
         ws.travelgoodbpel.TravelGoodBPELService service = new ws.travelgoodbpel.TravelGoodBPELService();
@@ -115,5 +115,5 @@ public class ClientTest {
         ws.travelgoodbpel.TravelGoodBPELPortType port = service.getTravelGoodBPELPortTypeBindingPort();
         return port.createItinerary(part1);
     }
-    
+
 }
